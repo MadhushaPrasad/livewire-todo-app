737a6f885df073abc2966c76ab9e07664d8c6e12
MadhushaPrasad <madushaprasad21@gmail.com>
2024-10-09 20:38:18 +0530

    read todo cards

--- app/Livewire/TodoList.php
+++ app/Livewire/TodoList.php
@@ -28,6 +28,8 @@ class TodoList extends Component
 
     public function render()
     {
-        return view('livewire.todo-list');
+        return view('livewire.todo-list', [
+            'todos' => Todo::latest()->get()
+        ]);
     }
 }
